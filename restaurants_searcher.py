443fb008c0a4a5a2e81fa07a1d0d8cc28a29e65c
kaori1997 <ikkaori7@gmail.com>
2021-02-13 13:51:47 +0900

    first commit

---
+++ restaurants_searcher.py
@@ -0,0 +1,32 @@
+# restaurants_searcher.py
+
+import json
+import csv
+import requests
+
+# 初期設定
+KEYID = "f40e71a94e9174411f16ede772229360"
+HIT_PER_PAGE = 100
+PREF = "PREF13"
+FREEWORD_CONDITION = 1
+FREEWORD = "渋谷駅"
+
+#パラメータ設定
+PARAMS = {"keyid": KEYID, "hit_per_page":HIT_PER_PAGE, "pref":PREF, "freeword_condition":FREEWORD_CONDITION, "freeword":FREEWORD}
+
+def write_data_to_csv(params):
+    restaurants = [["名称","住所","営業日","電話番号"]]
+    json_res = requests.get("https://api.gnavi.co.jp/RestSearchAPI/v3/", params=params).text
+    print(json_res)
+    response = json.loads(json_res)
+    if "error" in response:
+        return print("エラーが発生しました！")
+    for restaurant in response["rest"]:
+        rest_info = [restaurant["name"], restaurant["address"], restaurant["opentime"], restaurant["tel"]]
+        restaurants.append(rest_info)
+    with open("restaurants_list.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(restaurants)
+    return print(restaurants)
+
+write_data_to_csv(PARAMS)
